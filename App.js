b41e3d4973a9602719d216326ba0a619f21d25bf
Vlad Badea <vladbadea491@gmail.com>
2020-05-21 18:46:25 +0200

    a

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, Image } from 'react-native';
+import logo from './assets/logo.png';
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
@@ -9,9 +10,12 @@ const instructions = Platform.select({
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
+      <Image source={{uri: "https://i.imgur.com/TkIrScD.png"}} style={{width:305, height:159}} />
+      <Image source={logo} style={{width:305, height:159}} />
+
+      <Text style={{color:'#888', fontSize:18}}>
+        To share a photo from your phone with a friend, just press the button below!
+      </Text>
     </View>
   );
 }
